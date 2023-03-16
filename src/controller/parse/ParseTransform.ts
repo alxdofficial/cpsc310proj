30cbd284f21288e785a1d6b81c6066e474f4b88a
UBC Student <vleung20@students.cs.ubc.ca>
2023-03-16 11:01:38 -0700

    got rid of linter disables and changed switch line breaks

--- src/controller/parse/ParseTransform.ts
+++ src/controller/parse/ParseTransform.ts
@@ -3,23 +3,23 @@ import {QueryGroup} from "../output/QueryGroup";
 import {ParseTransformGroup} from "./ParseTransformGroup";
 import {QueryParser} from "./QueryParser";
 import {MFields, SFields} from "../query/InsightQuery";
-// eslint-disable-next-line import/namespace
 import {ParseTransformApply} from "./ParseTransformApply";
+
 export class ParseTransform {
 	public static parseTransform(json: any, parser: QueryParser,
 								 fieldsThatExistInOptions: Array<MFields | SFields>): Promise<Transformation | null> {
 		// first check if transform is specified
-		if (json["TRANSFORMATIONS"]  !== undefined) {
+		if (json["TRANSFORMATIONS"] !== undefined) {
 			let transformClause = json["TRANSFORMATIONS"];
 			let alreadyParsedApplyKeys: string[] = [];
 			let group: QueryGroup;
 			return ParseTransformGroup.parseTransformGroup(transformClause, parser).then((res) => {
 				group = res;
 			}).then(() => {
-				return ParseTransformApply.parseApply(transformClause,parser, alreadyParsedApplyKeys,
+				return ParseTransformApply.parseApply(transformClause, parser, alreadyParsedApplyKeys,
 					fieldsThatExistInOptions);
 			}).then((res) => {
-				return Promise.resolve(new Transformation(group,res));
+				return Promise.resolve(new Transformation(group, res));
 			}).catch((err) => {
 				return Promise.reject(err);
 			});
